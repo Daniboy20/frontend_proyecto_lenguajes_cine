4a2e73b8415a96947ce660b27d57d02bab07d3b0
EveeSC <evelynsarahi5ns@gmail.com>
2024-04-20 16:21:06 -0600

    ajustes de seleccion de botones css

--- resources/views/administradorsalas.blade.php
+++ resources/views/administradorsalas.blade.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/ba541049fa.js" crossorigin="anonymous"></script>
         <title>Administrar</title>
-        <link rel="stylesheet" href="{{ asset('css/peliculasstyle.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/salasstyle.css') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -21,7 +21,7 @@
                 </div>
                 <div id="body-options" class="elementsOption">
                     <a href="{{route('admin.peliculas')}}">
-                        <section id="peliculas-option" class="hover-effect">
+                        <section id="peliculas-option">
                             <i class="fa-solid fa-film"></i>
                             <span>Peliculas</span>
                         </section>
