18e8f38aa17722539cac5b4baeb545de7cc676cd
Daniboy20 <mejiadaniel898@gmail.com>
2024-04-21 18:11:49 -0600

    version anterior

--- app/Http/Controllers/BoletosController.php
+++ app/Http/Controllers/BoletosController.php
@@ -8,9 +8,34 @@ use GuzzleHttp\Client;
 class BoletosController extends Controller
 {
 
-    public function crearBoleto(Request $request)
+    public function crearBoleto($evento)
     {
+        /*
+        $idEvento = ;
+        $idAsiento = ;
+        $codigoDetalleFactura = ;
+        $client = new Client();
+        
+        try {
+            $response = $client->request('POST', 'http://localhost:8080/api/cliente/crear',
+                [
+                    'query' => [
+                        'idEvento' => $idEvento,
+                        'idAsiento' => $idAsiento,
+                        'codigoDetalleFactura' => $codigoDetalleFactura,
+                    ],
+                ]);
 
+                if($response->getStatusCode()==200){
+                    return redirect()->route('landingpage.home');
+                    //return view('landingpage');
+                    // return redirect()->route('landingpage');
+                }
+
+        } catch (\Exception $e) {
+            return response('Error' .$e->getMessage(), 500);
+        }
+        */
     }
 
 }
