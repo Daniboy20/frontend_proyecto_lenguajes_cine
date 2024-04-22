69450ec715d0a0a18627761a6ac35c81216173a3
Daniboy20 <mejiadaniel898@gmail.com>
2024-04-21 21:59:57 -0600

    nada

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -10,44 +10,25 @@ use GuzzleHttp\Promise;
 class FacturaController extends Controller
 {
     public function irCompras($codigoEvento, $codigoSala){
-        // http://localhost:8080/api/asientos/obtener?codigoSala=1
         $client = new Client();
-        try {
-            $response1 = $client->request('GET', 'http://localhost:8080/api/asientos/obtener?codigoSala='.$codigoSala);
     
-            if($response1->getStatusCode() == 200){
-                // Si da un 200 ok se almacena la respuesta en n¿una varuable
-                $contenido1 = $response1->getBody()->getContents();
-                // convierte el JSON a un array asociativo
-                $asientos = json_decode($contenido1, true);
-                return view('realizarCompra' , compact('codigoEvento', 'asientos'));
-            }
+        // Definir las promesas para las dos solicitudes
+        $promise1 = $client->getAsync('http://localhost:8080/api/asientos/obtener?codigoSala='.$codigoSala);
+        $promise2 = $client->getAsync('http://localhost:8080/api/boleto/obtenerPorEvento?codigoEvento='.$codigoEvento);
+    
+        try {
+            // Ejecutar las promesas de manera asíncrona
+            $results = Promise\Utils::settle([$promise1, $promise2])->wait();
+    
+            // Procesar los resultados de las promesas
+            $asientos = json_decode($results[0]['value']->getBody()->getContents(), true);
+            $boleto = json_decode($results[1]['value']->getBody()->getContents(), true);
+    
+            // Devolver la vista con los datos obtenidos
+            return view('realizarCompra', compact('codigoEvento', 'asientos', 'boletos'));
         } catch (\Exception $e) {
             // Manejo de errores
             return response('Error: ' . $e->getMessage(), 500);
         } 
-    
-        try {
-            $response2 = $client->request('GET', 'http://localhost:8080/api/boleto/obtenerPorEvento?codigoEvento='.$codigoEvento);
-    
-            if($response2->getStatusCode() == 200){
-                // Si da un 200 ok se almacena la respuesta en n¿una varuable
-                $contenido2 = $response2->getBody()->getContents();
-                // convierte el JSON a un array asociativo
-                $asientos = json_decode($contenido2, true);
-                return view('realizarCompra' , compact('codigoEvento', 'asientos'));
-            }
-        } catch (\Exception $e) {
-            // Manejo de errores
-            return response('Error: ' . $e->getMessage(), 500);
-        } 
-
-        $results = Promise\all([$response1, $response2])->wait();
-        $respuesta1 = $results[0];
-        $respuesta2 = $results[1]; 
-
-
-        
-        
     }
 }
